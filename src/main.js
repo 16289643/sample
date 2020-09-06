771d89248552a089d77bae2fb7d229ebf4605d41
16289643 <dialexval643@gmail.com>
2020-09-06 15:46:13 -0500

    Se actualiza rama Developer

--- src/main.js
+++ src/main.js
@@ -1,18 +1,28 @@
-let addNoteBtn = document.querySelector('#addaddNoteBtn');
+let addNoteBtn = document.querySelector('#addNoteBtn');
+addNoteBtn.onclick = () => {
 
-addNoteBtn.onclik = () => {
-
-    let newNoteInput = document.querySelector('#newNoteInput');
+    //input forms
+    let NewNoteInput = document.querySelector('#newNoteInput');
     let notes = document.querySelector('#app .notes')
 
-    //Nuevos Elementoss
+    //new elements
     let note = document.createElement('li');
     let checkbox = document.createElement('input');
 
-    checkbox.nodeType = "checkbox";
+    checkbox.type = "checkbox";
+
+    checkbox.onclick = function () {
+        let parent = this.parentElement;
+        if (this.checked) {
+            parent.classList.add('isDone');
+        } else {
+            parent.classList.remove('isDone');
+        }
+    }
+
     note.classList.add('note');
-    note.innerHTML = newNoteInput.values;
-    note.append(checkbox);
-    newNoteInput = '';
+    note.innerHTML = `<div>${newNoteInput.value}</div>`;
+    note.appendChild(checkbox);
+    newNoteInput.value = '';
     notes.appendChild(note);
 }
